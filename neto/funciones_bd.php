d1bd6e90ca1b07f04b3f9ef80ccb58593dc87c4e
Emmanuel Medina Martínez <cruguer96@gmail.com>
2020-02-05 15:26:28 -0600

    codigo inicial

---
+++ neto/funciones_bd.php
@@ -0,0 +1,67 @@
+<?php
+function conectar_bd() {
+	try {
+        $base=new PDO('mysql:host=localhost:8889; dbname=AudioElastixDialerBD', 'root', 'root');
+        $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $base->exec("SET CHARACTER SET UTF8");
+    } catch (Exception $e) {
+        die('Error' . $e->getMessage());
+        echo "Linea del error" . $e->getLine();
+    }
+    return $base;
+}
+
+function desconectar_bd($conector) {
+		mysql_close($conector);
+
+}
+
+function consultar_bd($sentencia) {
+	try {
+		$conector=conectar_bd();
+		$resultado = $conector->query($sentencia);
+		return $resultado;
+
+	} catch (Exception $e) {
+		die('Error' . $e->getMessage());
+        echo "Linea del error" . $e->getLine();
+	}
+
+}
+
+function insert_db($sentencia) {
+	try {
+		$conector = conectar_bd();
+		$prepare = $conector->prepare($sentencia);
+		$prepare->execute();
+	} catch(Exception $e) {
+		die('Error' . $e->getMessage());
+        echo "Linea del error" . $e->getLine();
+	}
+}
+
+function insertar_modificar_eliminar_bd($sentencia) {
+	$conector=conectar_bd();	
+	if(!($resultado=mysql_query($sentencia,$conector))) {
+		echo "Error en transacción de base de datos";
+		exit();
+	 }
+	 desconectar_bd($conector);
+}
+
+function calcular_nuevo_codigo_bd($nombretabla,$codigocampo) {
+	$sentencia="select max(".$codigocampo.") as num from ".$nombretabla.";";
+	$result=consultar_bd($sentencia);
+	$resultado = $result->fetchAll(PDO::FETCH_OBJ);
+	foreach ($resultado as $dato) {
+		echo $dato->num + 1;
+	}
+}
+
+?>
+
+
+
+
+
+
